003efa059d98b685bef16721bfa6ab3088c91b97
Rem-25 <1224909649@qq.com>
2026-03-26 22:01:46 +0800

    Create model.py

---
+++ 杨文峰/week09/model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.optim import Adam, SGD
+from torchcrf import CRF
+from transformers import BertModel
+
+class TorchModel(nn.Module):
+    def __init__(self, config):
+        super(TorchModel, self).__init__()
+        max_length = config['max_length']
+        class_num = config['class_num']
+        self.bert = BertModel.from_pretrained(config['bert_path'], return_dict=False)
+        self.classify =nn.Linear(self.bert.config.hidden_size, class_num)
+        self.crf_layer = CRF(class_num, batch_first=True)
+        self.use_crf = config['use_crf']
+        self.loss = torch.nn.CrossEntropyLoss(ignore_index=-1)
+
+    def forward(self, x, target = None):
+        x,_ = self.bert(x)
+        predict = self.classify(x)
+
+        if target is not None:
+            if self.use_crf:
+                mask = target.gt(-1) #创建布尔掩码，比较是否大于-1
+                return - self.crf_layer(predict, target, mask,reduction='mean') #大于-1的部分参与损失计算
+            else:
+                return self.loss(predict.view(-1,predict.shape[-1]), target.view(-1))
+        else:
+            if self.use_crf:
+                return self.crf_layer.decode(predict) #要用crf的解码，考虑了标签之间的发射概率和转移概率
+            else:
+                return predict
+
+def choose_optimizer(config, model):
+    optimizer = config['optimizer']
+    learning_rate = config['learning_rate']
+    if optimizer == 'adam':
+        return Adam(model.parameters(), lr=learning_rate)
+    elif optimizer == 'sgd':
+        return SGD(model.parameters(), lr=learning_rate)
+
+if __name__ == '__main__':
+    from config import Config
+    model = TorchModel(Config)
