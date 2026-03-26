5ee224ec6115ee106d8c0c869720e4bb077011f9
Rem-25 <1224909649@qq.com>
2026-03-26 22:00:43 +0800

    Update and rename ner_config.py to config.py

--- 杨文峰/week09/config.py
+++ 杨文峰/week09/config.py
@@ -1,24 +1,17 @@
-# -*- coding: utf-8 -*-
-
-"""
-配置参数信息
-"""
-
 Config = {
     "model_path": "model_output",
     "schema_path": "ner_data/schema.json",
     "train_data_path": "ner_data/train",
     "valid_data_path": "ner_data/test",
-    "vocab_size":9254,
     "vocab_path":"chars.txt",
     "max_length": 100,
     "hidden_size": 256,
+    "num_layers": 2,
     "epoch": 20,
     "batch_size": 16,
     "optimizer": "adam",
-    "learning_rate": 1e-3,
-    "use_crf": True,
+    "learning_rate": 1e-4,
+    "use_crf": False,
     "class_num": 9,
-    "bert_path": r"G:\AI\programs\bert-base-chinese"
+    "bert_path": r"G:\AI\programs\review\bert"
 }
-
