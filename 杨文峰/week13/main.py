5cbab23994ec092fc2c493700341d8a8d36d40be
Rem-25 <1224909649@qq.com>
2026-04-11 13:36:19 +0800

    Update main.py

--- 杨文峰/week13/main.py
+++ 杨文峰/week13/main.py
@@ -1,7 +1,5 @@
 import torch
 import os
-import random
-import os
 import numpy as np
 import logging
 from config import Config
@@ -9,39 +7,32 @@ from model import TorchModel, choose_optimizer
 from evaluate import Evaluator
 from loader import load_data
 from peft import get_peft_model, LoraConfig, TaskType
+#加入PEFT层
 
-
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
 
 def peft_wrapper(model):
     peft_config = LoraConfig(
-            r=8,
-            lora_alpha=32,
-            lora_dropout=0.1,
-            target_modules=["query", "value"]
-        )
+        r=8,
+        lora_alpha=32,
+        lora_dropout=0.1,
+        target_modules= ["query", "value"]
+    )
     return get_peft_model(model, peft_config)
 
 def main(config):
     if not os.path.isdir(config["model_path"]):
         os.mkdir(config["model_path"])
-    #训练数据
-    train_data = load_data(config["train_data_path"], config)
-    #模型
+    train_data = load_data(config["train_data_path"],config)
     model = TorchModel(config)
     model = peft_wrapper(model)
-    # 是否使用gpu
     cuda_flag = torch.cuda.is_available()
     if cuda_flag:
-        logger.info("gpu可以使用，迁移模型至gpu")
+        logger.info(f"cuda flag is {cuda_flag}")
         model = model.cuda()
-    #优化器
     optimizer = choose_optimizer(config, model)
-    #效果测试类
     evaluator = Evaluator(config, model, logger)
-    #训练
     for epoch in range(config["epoch"]):
         epoch += 1
         model.train()
@@ -58,12 +49,12 @@ def main(config):
             train_loss.append(loss.item())
             if index % int(len(train_data) / 2) == 0:
                 logger.info("batch loss %f" % loss)
-        logger.info("epoch average loss: %f" % np.mean(train_loss))
+        logger.info("epoch %d , average loss: %f" % epoch, np.mean(train_loss))
         evaluator.eval(epoch)
-    model_path = os.path.join(config["model_path"], "epoch_%d.pth" % epoch)
+    model_path = os.path.join(config["model_path"], "epoch_%d.pt" % epoch)
     torch.save(model.state_dict(), model_path)
-    
-    return model, train_data
+
+    return model,train_data
 
 if __name__ == "__main__":
     model, train_data = main(Config)
